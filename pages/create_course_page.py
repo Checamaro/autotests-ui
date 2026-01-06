60a81a7b1482eb0bab837f34026d8999179604ba
Checamaro <kirill.sharevich@yandex.ru>
2026-01-06 19:23:47 +0300

    Знакомство с PageFactory

--- pages/create_course_page.py
+++ pages/create_course_page.py
@@ -1,36 +1,29 @@
-from playwright.sync_api import Page, expect
-from pages.base_page import BasePage
-from components.navigation.navbar_component import NavbarComponent
+from playwright.sync_api import Page
+
+from components.courses.create_course_exercise_form_component import CreateCourseExerciseFormComponent
+from components.courses.create_course_exercises_toolbar_view_component import CreateCourseExercisesToolbarViewComponent
 from components.courses.create_course_form_component import CreateCourseFormComponent
 from components.courses.create_course_toolbar_view_component import CreateCourseToolbarViewComponent
-from components.courses.create_course_exercises_toolbar_view_component import CreateCourseExercisesToolbarViewComponent
-from components.views.image_upload_widget_component import ImageUploadWidgetComponent
+from components.navigation.navbar_component import NavbarComponent
 from components.views.empty_view_component import EmptyViewComponent
+from components.views.image_upload_widget_component import ImageUploadWidgetComponent
+from pages.base_page import BasePage
 
 
 class CreateCoursePage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        # Компоненты
         self.navbar = NavbarComponent(page)
         self.create_course_form = CreateCourseFormComponent(page)
-        self.create_course_toolbar_view = CreateCourseToolbarViewComponent(page)
-        self.create_course_exercises_toolbar_view = CreateCourseExercisesToolbarViewComponent(page)
         self.image_upload_widget = ImageUploadWidgetComponent(page, 'create-course-preview')
         self.exercises_empty_view = EmptyViewComponent(page, 'create-course-exercises')
-
-        self.create_course_title = page.get_by_test_id('create-course-toolbar-title-text')
-
-    def check_visible_create_course_title(self):
-        expect(self.create_course_title).to_be_visible()
-        expect(self.create_course_title).to_have_text('Create course')
+        self.create_course_toolbar_view = CreateCourseToolbarViewComponent(page)
+        self.create_course_exercise_form = CreateCourseExerciseFormComponent(page)
+        self.create_course_exercises_toolbar_view = CreateCourseExercisesToolbarViewComponent(page)
 
     def check_visible_exercises_empty_view(self):
         self.exercises_empty_view.check_visible(
             title='There is no exercises',
             description='Click on "Create exercise" button to create new exercise'
         )
-
-    def upload_preview_image(self, image_path: str):
-        self.image_upload_widget.upload_preview_image(image_path)
--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,19 +1,20 @@
-from playwright.sync_api import Page, expect
-from pages.base_page import BasePage
+from playwright.sync_api import Page
+
 from components.authentication.registration_form_component import RegistrationFormComponent
+from pages.base_page import BasePage
 
 
 class RegistrationPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
+
         self.registration_form = RegistrationFormComponent(page)
+
         self.login_link = page.get_by_test_id('registration-page-login-link')
+        self.registration_button = page.get_by_test_id('registration-page-registration-button')
+
+    def click_registration_button(self):
+        self.registration_button.click()
 
     def click_login_link(self):
         self.login_link.click()
-
-    def click_registration_button(self):
-        self.registration_form.registration_button.click()
-
-    def check_registration_button_to_be_enabled(self):
-        expect(self.registration_form.registration_button).to_be_enabled()
