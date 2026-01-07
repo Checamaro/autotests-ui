3d8e8ae38571f3dac0f8d9a3129a181716225675
Checamaro <kirill.sharevich@yandex.ru>
2026-01-07 16:42:36 +0300

    Практика работы с Page Factory

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -2,6 +2,7 @@ from playwright.sync_api import Page
 
 from components.authentication.registration_form_component import RegistrationFormComponent
 from pages.base_page import BasePage
+from elements.link import Link
 
 
 class RegistrationPage(BasePage):
@@ -10,11 +11,10 @@ class RegistrationPage(BasePage):
 
         self.registration_form = RegistrationFormComponent(page)
 
-        self.login_link = page.get_by_test_id('registration-page-login-link')
-        self.registration_button = page.get_by_test_id('registration-page-registration-button')
+        self.login_link = Link(page, 'registration-page-login-link', 'Login link')
 
     def click_registration_button(self):
-        self.registration_button.click()
+        self.registration_form.registration_button.click()
 
     def click_login_link(self):
         self.login_link.click()
